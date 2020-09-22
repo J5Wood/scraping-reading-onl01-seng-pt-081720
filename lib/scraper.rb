dd130a91bb5b3d8256dbf91635af1bd5820e75cb
Jeremy Wood <js.wood206@gmail.com>
2020-09-22 02:05:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
 
@@ -10,5 +11,6 @@ puts doc.css(".headline-26OIBN").text.strip
 courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|
+  binding.pry
   puts course.text.strip
 end
